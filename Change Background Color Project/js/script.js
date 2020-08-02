b54d153811c2ccd8f0bc97342ea195e1b0aca5b6
codersumon <mdsumon.rkb@gmail.com>
2020-08-03 01:31:39 +0600

    first projects update

--- Change Background Color Project/js/script.js
+++ Change Background Color Project/js/script.js
@@ -1,29 +1,12 @@
-//Choose a random color
-//var button = document.querySelector('button')
-//var body = document.querySelector('body')
-//var colors = ['red', 'green', 'blue', 'pink', 'red', 'purple']
-//
-//body.style.backgroundColor = 'orange'
-//button.addEventListener('click', changeBackground)
-//
-//function changeBackground() {
-//    var colorIndex = parseInt(Math.random() * colors.length)
-//    body.style.backgroundColor = colors[colorIndex]
-//}
-
-
-
-
+Choose a random color
 var button = document.querySelector('button')
 var body = document.querySelector('body')
-var colors = ['red', 'green', 'blue']
-
-
+var colors = ['red', 'green', 'blue', 'pink', 'red', 'purple']
 
 body.style.backgroundColor = 'orange'
 button.addEventListener('click', changeBackground)
 
-function changeBackground(){
-    var colorIndex = parseInt(Math.random()* colors.length)
+function changeBackground() {
+    var colorIndex = parseInt(Math.random() * colors.length)
     body.style.backgroundColor = colors[colorIndex]
 }
