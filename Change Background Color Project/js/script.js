d82450bf470435afe9eaf9577492b3e378384f1b
codersumon <mdsumon.rkb@gmail.com>
2020-08-03 01:14:16 +0600

    first projectst

---
+++ Change Background Color Project/js/script.js
@@ -0,0 +1,29 @@
+//Choose a random color
+//var button = document.querySelector('button')
+//var body = document.querySelector('body')
+//var colors = ['red', 'green', 'blue', 'pink', 'red', 'purple']
+//
+//body.style.backgroundColor = 'orange'
+//button.addEventListener('click', changeBackground)
+//
+//function changeBackground() {
+//    var colorIndex = parseInt(Math.random() * colors.length)
+//    body.style.backgroundColor = colors[colorIndex]
+//}
+
+
+
+
+var button = document.querySelector('button')
+var body = document.querySelector('body')
+var colors = ['red', 'green', 'blue']
+
+
+
+body.style.backgroundColor = 'orange'
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+    var colorIndex = parseInt(Math.random()* colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+}
